0d1d740234658a2ed46b0882bc1f4aa6e2ef0038
bobrito1004 <AlexLex2001@gmail.com>
2020-11-23 12:36:22 +0300

    lab3

---
+++ Lab3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Land = new Race<Vehicle>();
+            var car1 = new LandVehicle("camel", 20, 30, 5);
+            Land.Add(car1);
+            var boot = new Boots();
+            var broom = new Broom();
+            var mycar = new LandVehicle("BMW", 100, 0, 0);
+            Land.Add(broom);
+            Land.Add(boot);
+            Land.Add(mycar);
+            Console.WriteLine(Land.Start(100).Name);
+        }
+    }
+}
