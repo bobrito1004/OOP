9b3dcf8fc6e0463f0946e767969f6c71d2e4c7ab
bobrito1004 <AlexLex2001@gmail.com>
2020-11-23 12:38:13 +0300

    lab3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,12 +6,17 @@ namespace Lab3
     {
         public static void Main(string[] args)
         {
-            var Land = new Race<Vehicle>();
+            var All = new Race<Vehicle>();
+            var Land = new Race<LandVehicle>();
             var car1 = new LandVehicle("camel", 20, 30, 5);
-            Land.Add(car1);
             var boot = new Boots();
             var broom = new Broom();
             var mycar = new LandVehicle("BMW", 100, 0, 0);
+            All.Add(car1);
+            All.Add(broom);
+            All.Add(boot);
+            All.Add(mycar);
+            Land.Add(car1);
             Land.Add(broom);
             Land.Add(boot);
             Land.Add(mycar);
